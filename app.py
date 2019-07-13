899c9179c4ffeedf11eb02eba166b3c016f64c67
Anay Khator <anaykhator@gmail.com>
2019-07-13 18:47:25 +0530

    req raise

--- app.py
+++ app.py
@@ -53,3 +53,38 @@ def signup():
             return('Passwords did not match')
         db = SQL("sqlite:///P2P.db")
         db.execute("INSERT INTO users VALUES("+name+", "+id+", "+passw+", "+type+", "+email+", "+phone+", "+aadhaar+", "+pan+", "+dob+", "+ac+", "+ifsc+")")
+
+@app.route('/borDash', methods=['GET', 'POST'])
+def raise_req():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    elif request.method == 'POST':
+        data = request.form
+        select1 = request.form.get('tenure')
+        select2 = request.form.get('ltype')
+        borId=
+        Tenure=int(str(select1))
+        Ltype=str(select2)
+        amt=data['amt']
+        borscore=
+        borname=
+
+        
+
+
+        det = request.form
+        id = det['id']
+        p = det['pass']
+        # curr = mysql.connection.cursor()
+        # resultSet = curr.execute("SELECT * FROM login WHERE id='" + id + "'")
+        # if resultSet == 1:
+        #     ad = curr.fetchall()
+        #     if ad[0][2] == p:
+        #         if ad[0][3] == 'b':
+        #             return redirect("/borDash")
+        #         elif ad[0][3] == 'l':
+        #             return redirect("/lenDash")
+        #     else:
+        #         flash("Passwords do not match", "danger")
+        #         return redirect("/home")
+        return render_template('home.html')
