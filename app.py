9f011cef6b9c12bf00fb10fe419241aedef0e739
kd411 <urwithshanu@gmail.com>
2019-07-13 14:45:18 +0530

    Database config

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, render_template, redirect, flash, session
-from flask_mysqldb import MySQL
+from cs50 import SQL
 
 app = Flask(__name__)
-mysql = MySQL(app)
 
 
 @app.route('/')
@@ -13,18 +12,34 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        return render_template('signup.html')
     elif request.method == 'POST':
         det = request.form
         id = det['id']
         p = det['pass']
+        curr = mysql.connection.cursor()
+        resultSet = curr.execute("SELECT * FROM login WHERE id='" + id + "'")
+        if resultSet == 1:
+            ad = curr.fetchall()
+            if ad[0][2] == p:
+                if ad[0][3] == 'b':
+                    return redirect("/borDash")
+                elif ad[0][3] == 'l':
+                    return redirect("/lenDash")
+            else:
+                flash("Passwords do not match", "danger")
+                return redirect("/home")
+        return render_template('home.html')
 
 
-@app.route('/borrowersignup')
-def borsign():
-    return render_template('borSign.html')
-
-
-@app.route('/lendersignup')
-def lensign():
-    return render_template('lendSign.html')
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        data = request.form
+        name = data['name']
+        phone = data['phone']
+        email = data['email']
+        aadhaar = data['aadhaar']
+        pan = data['pan']
+        passw = data['pass']
+        repass = data['repass']
