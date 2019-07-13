e48c72b34155d857386075195e33b21f8e8f9f15
ansh <ansharora2299@gmail.com>
2019-07-13 13:16:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, session, g
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
