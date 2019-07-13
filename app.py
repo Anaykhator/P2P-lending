c1e100d397508da33aa1f085fe69b294081441db
ansh <ansharora2299@gmail.com>
2019-07-13 18:56:32 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,104 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from cs50 import SQL
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    elif request.method == 'POST':
+        det = request.form
+        id = det['id']
+        p = det['pass']
+        return render_template('home.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+<<<<<<< HEAD
+    # if request.method == 'POST':
+    data = request.form
+    name = data['name']
+    phone = data['phone']
+    email = data['email']
+    aadhaar = data['aadhaar']
+    pan = data['pan']
+    dob = data['dob']
+    ac = data['ac']
+    ifsc = data['ifsc']
+    userid = data['id']
+    passw = data['pass']
+    repass = data['repass']
+    type = data['mode']
+    if passw != repass:
+        return sorry("invalid username and/or password")
+    return sorry("Successful")
+        # db = SQL("sqlite:///P2P.db")
+        # db.execute("INSERT INTO users VALUES('"+name+"','"+userid+"','"+passw+"','"+type+"','"+email+"','"+phone+"','"+aadhaar+"','"+pan+"','"+dob+"','"+ac+"','"+ifsc+"')")
+
+def sorry(message):
+    flash(message)
+    return render_template("sorry.html")
+=======
+    if request.method == 'POST':
+        data = request.form
+        name = data['name']
+        phone = data['phone']
+        email = data['email']
+        aadhaar = data['aadhaar']
+        pan = data['pan']
+        dob = data['dob']
+        ac = data['ac']
+        ifsc = data['ifsc']
+        userid = data['id']
+        passw = data['pass']
+        repass = data['repass']
+        type = data['mode']
+        if passw != repass:
+            return('Passwords did not match')
+        db = SQL("sqlite:///P2P.db")
+        db.execute("INSERT INTO users VALUES("+name+", "+id+", "+passw+", "+type+", "+email+", "+phone+", "+aadhaar+", "+pan+", "+dob+", "+ac+", "+ifsc+")")
+
+@app.route('/borDash', methods=['GET', 'POST'])
+def raise_req():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    elif request.method == 'POST':
+        data = request.form
+        select1 = request.form.get('tenure')
+        select2 = request.form.get('ltype')
+        borId=
+        Tenure=int(str(select1))
+        Ltype=str(select2)
+        amt=data['amt']
+        borscore=
+        borname=
+
+        
+
+
+        det = request.form
+        id = det['id']
+        p = det['pass']
+        # curr = mysql.connection.cursor()
+        # resultSet = curr.execute("SELECT * FROM login WHERE id='" + id + "'")
+        # if resultSet == 1:
+        #     ad = curr.fetchall()
+        #     if ad[0][2] == p:
+        #         if ad[0][3] == 'b':
+        #             return redirect("/borDash")
+        #         elif ad[0][3] == 'l':
+        #             return redirect("/lenDash")
+        #     else:
+        #         flash("Passwords do not match", "danger")
+        #         return redirect("/home")
+        return render_template('home.html')
+>>>>>>> 899c9179c4ffeedf11eb02eba166b3c016f64c67
