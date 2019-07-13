a506545ea6f6d611cc36f33904c290761bcdcce2
ansh <ansharora2299@gmail.com>
2019-07-13 19:04:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,6 @@ def login():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-<<<<<<< HEAD
-    # if request.method == 'POST':
     data = request.form
     name = data['name']
     phone = data['phone']
@@ -47,25 +45,6 @@ def signup():
 def sorry(message):
     flash(message)
     return render_template("sorry.html")
-=======
-    if request.method == 'POST':
-        data = request.form
-        name = data['name']
-        phone = data['phone']
-        email = data['email']
-        aadhaar = data['aadhaar']
-        pan = data['pan']
-        dob = data['dob']
-        ac = data['ac']
-        ifsc = data['ifsc']
-        userid = data['id']
-        passw = data['pass']
-        repass = data['repass']
-        type = data['mode']
-        if passw != repass:
-            return('Passwords did not match')
-        db = SQL("sqlite:///P2P.db")
-        db.execute("INSERT INTO users VALUES("+name+", "+id+", "+passw+", "+type+", "+email+", "+phone+", "+aadhaar+", "+pan+", "+dob+", "+ac+", "+ifsc+")")
 
 @app.route('/borDash', methods=['GET', 'POST'])
 def raise_req():
@@ -81,24 +60,3 @@ def raise_req():
         amt=data['amt']
         borscore=
         borname=
-
-        
-
-
-        det = request.form
-        id = det['id']
-        p = det['pass']
-        # curr = mysql.connection.cursor()
-        # resultSet = curr.execute("SELECT * FROM login WHERE id='" + id + "'")
-        # if resultSet == 1:
-        #     ad = curr.fetchall()
-        #     if ad[0][2] == p:
-        #         if ad[0][3] == 'b':
-        #             return redirect("/borDash")
-        #         elif ad[0][3] == 'l':
-        #             return redirect("/lenDash")
-        #     else:
-        #         flash("Passwords do not match", "danger")
-        #         return redirect("/home")
-        return render_template('home.html')
->>>>>>> 899c9179c4ffeedf11eb02eba166b3c016f64c67
